f80cdbc96abeb9b88e7ab19d2ae3b0cec4760e3a
ewex25 <ewex258@gmail.com>
2022-06-02 13:55:08 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#include stdio.h
-
+#include <stdio.h>
 /**
- * main - function
- *
- * Description: this is awesome
- *
- * return: returns 0
- *
- */
+ *  * main - function
+ *   * Description: this is fun
+ *    * Return: 0 if successful
+ *     */
 int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle');
-	return(0); 
-}
+{ puts("\"Programming is like building a multilingual puzzle");
+	return (0); }
+
