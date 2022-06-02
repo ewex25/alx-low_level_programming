449f5f67acb53227238e582cd69395154e7f069d
ewex25 <ewex258@gmail.com>
2022-06-02 15:21:52 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Description: this will printf
+ * Return: Description of the return bruv
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
