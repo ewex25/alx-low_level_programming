e506078ed208d04623ddad0c5b486368d7c53d0b
ewex25 <ewex258@gmail.com>
2022-06-02 17:11:49 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <unistd.h>
+#include<stdio.h>
+#include<unistd.h>
 /**
-* main: entry point
-* Description:; am chilling with this C
-* Return: 1
-*/
+ * main - Entry point
+ * Return: 1
+ */
 int main(void)
-{	
-	 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	 return (1);
-}	 
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
