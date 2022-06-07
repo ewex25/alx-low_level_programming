ea83d28600a26713df19b13f73fdf20867266e44
ewex25 <ewex258@gmail.com>
2022-06-07 10:08:14 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints putchar followed ny a new line
+ * Description: prints putchar followed by a newline
+ * Return:0
+ */
+int main(void)
+
+{
+	write(1, "_putcher", 57);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+my nested loops and function project
