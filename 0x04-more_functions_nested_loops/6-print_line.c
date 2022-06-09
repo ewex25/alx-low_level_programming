7016d2e8efcc179fa466269de48f783ac4532127
ewex25 <ewex258@gmail.com>
2022-06-09 15:49:49 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,7 +5,7 @@
  * print_line - draws a straight line of length n in the terminal
  * followed by a new line
  *
- * @n: the number of times the character _ should be printed
+ * main: the number of times the character _ should be printed
  *
  * Description: If n is 0 or less, the function should only print ''\n'
  *
