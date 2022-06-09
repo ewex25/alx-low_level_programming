99edc920fe51b9d236732a10fa10bf604fd62fb6
ewex25 <ewex258@gmail.com>
2022-06-09 15:24:49 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,19 @@
 #include "main.h"
-
 /**
- * print_numbers - prints the numbers, from 0 to 9, followed by a new line
- *
- **/
-
-void print_numbers(void)
+ * print_most_numbers - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
 {
-	int c;
+char c = '0';
 
-	for (c = 0; c < 10; c++)
-	{
-		if (c != 2 && c != 4)
-		{
-
-			_putchar(c + '0');
-		}
-	}
-
-	_putchar('\n');
+while (c <= '9')
+{
+if (c != '2' && c != '4')
+{
+_putchar(c);
+}
+c++;
+}
+_putchar('\n');
 }
