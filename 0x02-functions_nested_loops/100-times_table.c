7f66e750f7b4bfe1cb26cb05769ad54ecedd1083
ewex25 <ewex258@gmail.com>
2022-06-12 06:05:46 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,66 +1,47 @@
 #include "main.h"
-
-int _putchar(char c);
-
 /**
- * print_times_table - print the times table until n
- *
- * @n: n times table
- *
- * Return: No return
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: Nothing.
  */
-
 void print_times_table(int n)
 {
-	int i;
-	int j;
+	int i, j, res;
 
-	if (n >= 0 && n <= 15)
+	if (!(n > 15 || n < 0))
 	{
-
 		for (i = 0; i <= n; i++)
 		{
-
 			for (j = 0; j <= n; j++)
 			{
-
-				if ((i * j) < 10)
+				res = (i * j);
+				if (j != 0)
 				{
-					if (j != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar((j * i) + '0');
+					_putchar(',');
+					_putchar(' ');
 				}
-
-				else if ((i * j) < 100)
+				if (res < 10 && j != 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((((j * i) / 10) % 10) + '0');
-					_putchar(((j * i) % 10) + '0');
+					_putchar((res % 10) + '0');
 				}
-
-				else if ((i * j) < 1000)
+				else if (res >= 10 && res < 100)
 				{
 					_putchar(' ');
-					_putchar((((j * i) / 100) % 10) + '0');
-					_putchar((((j * i) / 10) % 10) + '0');
-					_putchar(((j * i) % 10) + '0');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
 				}
-			if (j != (n))
-			{
-				_putchar(',');
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
 			}
-
-			}
-
-		_putchar('\n');
-
+			_putchar('\n');
 		}
-
 	}
-
 }
