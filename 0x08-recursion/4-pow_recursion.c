f170f43de955b77175eea4d4d24a7102089f3546
ewex25 <ewex258@gmail.com>
2022-06-22 12:25:34 -0700

    tasks

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,18 @@
-#include "holberton.h"
+#include "main.h"
+#include <stddef.h>
+
 /**
- *_pow_recursion - returns x to power y
+ * _pow_recursion - returns the x to the power y
+ * @x: the base
+ * @y: the exponent
  *
- *@x:value
- *@y:power
- *Return:(-1) < 0,value
+ * Return: the integer result, -1 on error
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
-	if (y == 0)
-	{
-		return (1);
-	}
-	return (x * _pow_recursion(x, y - 1));
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	return (1);
+}
