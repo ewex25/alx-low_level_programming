a63d8d52cb268170f6f517ae284f893a39844bf2
ewex25 <ewex258@gmail.com>
2022-06-30 15:02:50 -0700

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,49 +1,51 @@
-#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
+#include "stdio.h"
+
 /**
-*
-realloc
-*
-@ptr:
-* @old size:
-* @new size:
-*
-* Return:
-* /
-void
-*_realloc(void *ptr, unsigned int old_size, unsigned
-int new_size)
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ *
+ * Return: pointer to the newly allocated memory block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *nptr;
-unsigned int i;
-if (new_size
-== old size)
-return (ptr):
-if (ptr
-== NULL)
-{
-nptr = malloc(new_size);
-if (nptr == NULL)
-return (NULL);
-return (nptr);
-}
-else
-{
-if (new size == 0)
-{
-free(ptr);
-return (NULL);
-7
-7
-nptr = malloc(new_size);
-if (nptr == NULL)
-return (NULL);
-for (i = 0; i
-{
-< old_size && i < new_size; i++)
-nptr[i] = ((char *) ptr)li];
-}
-free(ptr);
-return (nptr);
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
 }
