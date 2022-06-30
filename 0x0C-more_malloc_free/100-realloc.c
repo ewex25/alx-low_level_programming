471381901cdeb55d3f285199bd5972bdc8847a7c
ewex25 <ewex258@gmail.com>
2022-06-30 14:51:38 -0700

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,51 @@
+#include
+"main.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include
+‹stdio.h>
 /**
- *_realloc - reallocates memory using malloc and free
- *@ptr:previous mem
- *@old_size:
+*
+realloc
+*
+@ptr:
+* @old size:
+* @new size:
+*
+* Return:
+* /
+void
+*_realloc(void *ptr, unsigned int old_size, unsigned
+int new_size)
+{
+char *nptr;
+unsigned int i;
+if (new_size
+== old size)
+return (ptr):
+if (ptr
+== NULL)
+{
+nptr = malloc(new_size);
+if (nptr == NULL)
+return (NULL);
+return (nptr);
+}
+else
+{
+if (new size == 0)
+{
+free(ptr);
+return (NULL);
+7
+7
+nptr = malloc(new_size);
+if (nptr == NULL)
+return (NULL);
+for (i = 0; i
+{
+< old_size && i < new_size; i++)
+nptr[i] = ((char *) ptr)li];
+}
+free(ptr);
+return (nptr);
+}
