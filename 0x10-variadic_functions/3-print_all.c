d29f8abea9209af8956e21bf09f47c1fd102ef20
ewex25 <ewex258@gmail.com>
2022-07-07 09:44:39 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,86 +1,53 @@
-#include "variadic_functions.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <stdarg.h>
 /**
- * print_int - prints int
- * @list: arguments from print_all
+ *print_all - prints any type of character
+ *
+ *@format:format
+ *
+ *Return:void
  */
-void print_int(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-
-/**
- * print_float - prints float
- * @list: arguments from print_all
- */
-void print_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-
-/**
- * print_char - prints int
- * @list: arguments from print_all
- */
-void print_char(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-
-/**
- * print_str - prints string
- * @list: arguments from print_all
- */
-void print_str(va_list list)
-{
-	char *s = va_arg(list, char *);
-
-	s == NULL ? printf("(nil)") : printf("%s", s);
-
-}
-
-/**
- * print_all - prints any type
- * @format: arguments to print
- */
-
 void print_all(const char * const format, ...)
 {
-va_list list;
-int i = 0, j = 0;
-char *sep = "";
+	int i = 0;
+	char *str, *sprtr = "";
 
-printTypeStruct printType[] = {
-	{ "i", print_int },
-	{ "f", print_float },
-	{ "c", print_char },
-	{ "s", print_str },
-	{NULL, NULL}
-};
+	va_list va;
 
+	va_start(va, format);
 
-va_start(list, format);
-
-while (format && format[i])
-{
-	j = 0;
-	while (j < 4)
+	if (format)
 	{
-		if (*printType[j].type == format[i])
+		while (format[i] != '\0')
 		{
-			printf("%s", sep);
-			printType[j].printer(list);
-			sep = ", ";
-			break;
+			switch (format[i])
+			{
+			case 'c':
+				printf("%s%c", sprtr, va_arg(va, int));
+				break;
+			case 'i':
+				printf("%s%d", sprtr, va_arg(va, int));
+				break;
+			case 'f':
+				printf("%s%f", sprtr, va_arg(va, double));
+				break;
+			case 's':
+				str = va_arg(va, char *);
+				if (str == NULL)
+				{
+					str = "(nil)";
+				}
+				printf("%s%s", sprtr, str);
+				break;
+			default:
+				i++;
+				continue;
+			}
+			sprtr = ", ";
+			i++;
 		}
-		j++;
 	}
-	i++;
+	printf("\n");
+	va_end(va);
 }
-
-printf("\n");
-va_end(list);
-}
-
