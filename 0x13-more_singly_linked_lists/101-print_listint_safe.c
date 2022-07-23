8bcd44738c16a589854fdbb02b379a823e10785e
ewex25 <ewex258@gmail.com>
2022-07-23 06:38:15 -0700

    tasks2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,56 +1,88 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
+
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
 /**
- *unique_node_count - counts no. of unique nodes
- *@head:pointer to a head node
- *Return:number of unique nodes,otherwise 0
+ * looped_listint_len - Counts the number of unique nd
+ *                      in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nd in the list.
  */
-size_t unique_node_count(const listint_t *head)
+size_t looped_listint_len(const listint_t *head)
 {
-	listint_t *hare, *tortoise;
-	size_t count = 1;
+const listint_t *eli, *bun;
+size_t nd = 1;
 
-	if (head == NULL || head->next == NULL)
-	{
-		return (0);
-	}
-	hare = head->next->next;
-	tortoise = head->next;
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				count++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				count++;
-				tortoise = tortoise->next;
-			}
+if (head == NULL || head->next == NULL)
+return (0);
+
+eli = head->next;
+bun = (head->next)->next;
+
+while (bun)
+{
+if (eli == bun)
+{
+eli = head;
+while (eli != bun)
+{
+nd++;
+eli = eli->next;
+bun = bun->next;
+}
+
+eli = eli->next;
+while (eli != bun)
+{
+nd++;
+eli = eli->next;
+}
+
+return (nd);
+}
+
+eli = eli->next;
+bun = (bun->next)->next;
+}
+
+return (0);
+}
 
 /**
- *print_listint_safe - prints a listint list
- *@head:pointer to head
- *Return:number of nodes in list
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nd in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	unsigned int i = 0;
+size_t nd, index = 0;
 
-	if (head == NULL)
-		exit(98);
-	while (head)
-	{
-		printf("[%p] %d", (void *)head, head->n);
-		i++;
-		head = head->next;
-	}
-	return (i);
+nd = looped_listint_len(head);
+
+if (nd == 0)
+{
+for (; head != NULL; nd++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+}
+
+else
+{
+for (index = 0; index < nd; index++)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+}
+
+printf("-> [%p] %d\n", (void *)head, head->n);
+}
+
+return (nd);
 }
