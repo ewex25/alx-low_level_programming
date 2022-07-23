9890bba1735f1cf4311efb98be8fc78252c1c9f1
ewex25 <ewex258@gmail.com>
2022-07-23 05:09:55 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,38 +1,37 @@
 #include "main.h"
-#include <stddef.h>
+
 /**
- * binary_to_uint - Converts a binary number to an unsigned integer
- * @b: String representing a binary number
- * Return: The converted number or 0 if b is NULL or if there are chars
- * that are not 0 or 1
- */
+* binary_to_uint - converts a binary number to an
+* unsigned int.
+* @b: binary.
+*
+* Return: unsigned int.
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int a;
-	unsigned int f;
-	unsigned int d;
-	unsigned int sum;
-	unsigned int exp;
-	int y;
+unsigned int ui;
+int len, base_two;
 
-	y = 2;
-	sum = 0;
-	exp = 1;
-	if (b == NULL)
-		return (0);
-	for (a = 0; b[a] != '\0'; a++) /*Getting the length of b num*/
-		;
-	if (a == 1 && (b[0] == '0' || b[0] == '1')) /*Checking single nums*/
-		return ((b[0] - 48));
-	for (f = 0; b[f] != '\0'; f++) /*Iterating the string*/
-	{
-		if (b[f] != '0' && b[f] != '1')
-			return (0);
-		for (d = a - 1; d > 0; d--) /*Calculating the exponent*/
-			exp = exp * y;
-		sum = sum + (exp * (b[f] - 48)); /*Adding the number*/
-		a--;
-		exp = 1; /*Reseting the exponent*/
-	}
-	return (sum);
+if (!b)
+return (0);
+
+ui = 0;
+
+for (len = 0; b[len] != '\0'; len++)
+;
+
+for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+{
+if (b[len] != '0' && b[len] != '1')
+{
+return (0);
+}
+
+if (b[len] & 1)
+{
+ui += base_two;
+}
+}
+
+return (ui);
 }
